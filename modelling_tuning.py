e0e53e60e475e8ff948dc795c56664ad6d3ea199
Hilmi Datu Allam <hilmi.allam@gmail.com>
2025-11-23 16:00:03 +0700

    Update modelling_tuning.py
    Melakukan modulariasasi
    Menggunakan optuna manual agar masih dalam rana logging manual tanpa autolog maupun callback dari optuna

--- modelling_tuning.py
+++ modelling_tuning.py
@@ -1,14 +1,27 @@
 import mlflow
+import optuna
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import precision_score, recall_score, f1_score, confusion_matrix
-import random
-import numpy as np
+from sklearn.metrics import (
+    accuracy_score, precision_score, recall_score, f1_score,
+    roc_auc_score, log_loss, confusion_matrix,
+    roc_curve, precision_recall_curve, auc
+)
+from mlflow.models import infer_signature
+import json
+import os
+os.environ["MPLBACKEND"] = "Agg"
+import warnings
+warnings.filterwarnings("ignore")
+import matplotlib.pyplot as plt
+import seaborn as sns
+#Kalau mau pake dagshub
 import dagshub
 dagshub.init(repo_owner='d4tu4llam', repo_name='Workflow-CI', mlflow=True)
-#tracking UI
-mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+mlflow.set_tracking_uri("https://dagshub.com/d4tu4llam/Workflow-CI.mlflow")
+
+#tracking UI Local
+#mlflow.set_tracking_uri("http://127.0.0.1:5000/")
 mlflow.set_experiment("Submission SML Modelling Tuning Diabetes Prediction Hilmi Datu Allam")
 train_data = pd.read_csv("diabetes_prediction_dataset_preprocessing/train_processed.csv")
 test_data = pd.read_csv("diabetes_prediction_dataset_preprocessing/test_processed.csv")
@@ -24,46 +37,252 @@ y_test = test_data["diabetes"]
 #Ukuran input
 input_example = X_train[0:5]
 
-#Parameter untuk utning
-n_estimators_range = np.linspace(10,300, 5, dtype=int) #akan naik incremental 5
-max_depth_range = np.linspace(1,50, 5, dtype=int)#akan naik incremental 5
-best_accuracy = 0
-best_params={}
+def log_all_metrics(y_train_pred, y_train_proba, y_test_pred ,y_test_proba ):
+    # Log all metrics
+    mlflow.log_metrics({
+        "train_accuracy": accuracy_score(y_train, y_train_pred),
+        "train_precision": precision_score(y_train, y_train_pred),
+        "train_recall": recall_score(y_train, y_train_pred),
+        "train_f1": f1_score(y_train, y_train_pred),
+        "train_auc": roc_auc_score(y_train, y_train_proba),
+        "test_accuracy": accuracy_score(y_test, y_test_pred),
+        "test_precision": precision_score(y_test, y_test_pred),
+        "test_recall": recall_score(y_test, y_test_pred),
+        "test_f1": f1_score(y_test, y_test_pred),
+        "test_auc": roc_auc_score(y_test, y_test_proba),
+        "test_log_loss": log_loss(y_test, y_test_proba),
+    })
+ 
+def log_metric_info():
+    #Buat metric_info.json
+    metric_info = {
+        "metrics": [
+            {"name": "test_accuracy",        "type": "scalar"},
+            {"name": "test_precision",       "type": "scalar"},
+            {"name": "test_recall",          "type": "scalar"},
+            {"name": "test_f1",              "type": "scalar"},
+            {"name": "test_auc",             "type": "scalar"},
+            {"name": "test_log_loss",        "type": "scalar"},
+            {"name": "training_accuracy",    "type": "scalar"},
+            {"name": "training_precision",   "type": "scalar"},
+            {"name": "training_recall",      "type": "scalar"},
+            {"name": "training_f1",          "type": "scalar"},
+            {"name": "training_auc",         "type": "scalar"},
+            {"name": "training_log_loss",    "type": "scalar"},
+            {"name": "RandomForestClassifier_score_X_test", "type": "scalar"}
+        ]
+    }
+
+    # Simpan ke file JSON
+    with open("metric_info.json", "w") as f:
+        json.dump(metric_info, f, indent=2)
+
+    # Log ke MLflow
+    mlflow.log_artifact("metric_info.json")
+    os.remove("metric_info.json")  
+
+def log_estimator_html(model):
+    #Buat estimator.html
+    html_content = model._repr_html_()   
+
+    # Simpan ke file
+    with open("estimator.html", "w", encoding="utf-8") as f:
+        f.write(html_content)
+        
+    # Log ke MLflow
+    mlflow.log_artifact("estimator.html")
+    os.remove("estimator.html")
+
+def log_visual(model, X_test, y_test_pred, y_test_proba):
+    # estimator.html
+    with open("estimator.html", "w", encoding="utf-8") as f:
+        f.write(model._repr_html_())
+    mlflow.log_artifact("estimator.html")
+    os.remove("estimator.html")
+
+    # Confusion Matrix
+    cm = confusion_matrix(y_test, y_test_pred)
+    plt.figure(figsize=(6,5))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',
+                xticklabels=['No Diabetes', 'Diabetes'],
+                yticklabels=['No Diabetes', 'Diabetes'])
+    plt.title(f"Confusion Matrix (")
+    plt.ylabel("True"); plt.xlabel("Predicted")
+    plt.tight_layout()
+    plt.savefig(f"confusion_matrix.png"); plt.close()
+    mlflow.log_artifact(f"confusion_matrix.png")
+    os.remove(f"confusion_matrix.png")
+
+    # ROC Curve
+    fpr, tpr, _ = roc_curve(y_test, y_test_proba)
+    roc_auc_val = auc(fpr, tpr)
+    plt.figure(figsize=(7,6))
+    plt.plot(fpr, tpr, label=f'ROC Curve (AUC = {roc_auc_val:.3f})')
+    plt.plot([0,1], [0,1], 'k--')
+    plt.xlabel('FPR'); plt.ylabel('TPR'); plt.title('ROC Curve')
+    plt.legend(); plt.grid(True); plt.tight_layout()
+    plt.savefig(f"roc_curve.png"); plt.close()
+    mlflow.log_artifact(f"roc_curve.png")
+    os.remove(f"roc_curve.png")
+
+    # Precision-Recall Curve
+    precision, recall, _ = precision_recall_curve(y_test, y_test_proba)
+    pr_auc = auc(recall, precision)
+    plt.figure(figsize=(7,6))
+    plt.plot(recall, precision, label=f'PR Curve (AUC = {pr_auc:.3f})')
+    plt.xlabel('Recall'); plt.ylabel('Precision'); plt.title('Precision-Recall Curve')
+    plt.legend(); plt.grid(True); plt.tight_layout()
+    plt.savefig(f"precision_recall_curve.png"); plt.close()
+    mlflow.log_artifact(f"precision_recall_curve.png")
+    os.remove(f"precision_recall_curve.png")
 
 
 
-for n_estimators in n_estimators_range:
-    for max_depth in max_depth_range:
-        with mlflow.start_run(run_name =f"elastic_search_{n_estimators}_{max_depth}"):
-            mlflow.log_param("n_estimators", n_estimators)
-            mlflow.log_param("max_depth", max_depth if max_depth else "None")
-            mlflow.log_param("random_state", 42)
-            mlflow.log_param("tuning_method", "Manual Grid Search")
-            #Train model
-            model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state =42)
-            model.fit(X_train, y_train)
+def objective(trial):
+    """
+    Objective function untuk Optuna Hyperparameter Tuning
+    
+    Objective: Memaksimalkan skor gabungan (Custom Weighted Score)
+               yang mengutamakan Recall (keselamatan pasien) sambil
+               menjaga Precision agar tidak jatuh terlalu rendah (efisiensi biaya).
+    
+    Custom Score yang digunakan:
+        score = 0.8 x Recall + 0.2 x Precision
+    
+    Alasan pemilihan bobot:
+        - Recall diberi bobot 80% karena:
+              False Negative (melewatkan penderita diabetes) 
+              dapat berakibat fatal 
+        - Precision diberi bobot 20% karena:
+              False Positive yang terlalu banyak menyebabkan 
+              biaya screening ulang tinggi dan beban sistem kesehatan
+    
+    Pendekatan ini merupakan kompromi optimal antara:
+        - Recall dan Precision untuk konteks medis diabetes.
+    
+    Metrik yang di-log:
+        - custom_recall_precision_score : nilai objective (0.8R + 0.2P)
+        - test_recall, test_precision   : untuk monitoring detail
+        - semua metrik standar via log_all_metrics()
+    
+    Return:
+        float: custom_recall_precision_score (nilai yang dioptimasi Optuna)
+    """
+    n_estimators = trial.suggest_int("n_estimators", 100, 1000)
+    max_depth = trial.suggest_int("max_depth", 8, 40)
+    max_depth = None if max_depth >= 35 else max_depth
+    min_samples_split = trial.suggest_int("min_samples_split", 2, 20)
+    min_samples_leaf = trial.suggest_int("min_samples_leaf", 1, 10)
+    max_features = trial.suggest_categorical("max_features", ["sqrt", "log2", 0.5, 0.8])
+    bootstrap = trial.suggest_categorical("bootstrap", [True, False])
+    class_weight = trial.suggest_categorical("class_weight", ["balanced",None,{0:1, 1:3},{0:1, 1:4},{0:1, 1:5},{0:1, 1:6}] )
 
-            #Evaluate model
-            predictions = model.predict(X_test)
-            accuracy= model.score(X_test, y_test)
-            precision = precision_score(y_test, predictions)
-            recall = recall_score(y_test, predictions)
-            f1 = f1_score(y_test, predictions)
+
+    params = {
+        "n_estimators": n_estimators,
+        "max_depth": max_depth,
+        "min_samples_split": min_samples_split,
+        "min_samples_leaf": min_samples_leaf,
+        "max_features": max_features,
+        "random_state":42,
+        "n_jobs": -1,
+        "bootstrap": bootstrap,
+        "class_weight": class_weight,
+        "criterion": "gini"
+            }
+
+    # Setiap trial jadi nested run
+    with mlflow.start_run(nested=True, run_name=f"Trial_{trial.number}"):
+        mlflow.log_params(params)
+        mlflow.log_param("optuna_trial_number", trial.number)
+        #Train model
+
+        model = RandomForestClassifier(**params)
+        model.fit(X_train, y_train)
+        
+        #Training Metrics
+        y_train_pred = model.predict(X_train)
+        y_train_proba = model.predict_proba(X_train)[:, 1]
+        #Test metrics
+        y_test_pred = model.predict(X_test)
+        y_test_proba = model.predict_proba(X_test)[:, 1]
+
+        recall = recall_score(y_test, y_test_pred)
+        precision = precision_score(y_test, y_test_pred)
+
+        # score custom karena kalau mementingkan recall tadi precisionnya drop ke 0.4
+        score = 0.8 * recall + 0.2 * precision   
+        mlflow.log_metric("custom_recall_precision_score", score)
+        #Log all metrics
+        log_all_metrics(y_train_pred, y_train_proba, y_test_pred, y_test_proba)
+        #Log metric info
+        log_metric_info()
+        #Log Estimator
+        log_estimator_html(model)
+        #Log visual seperti cm roc auc 
+        log_visual(model, X_test, y_test_pred, y_test_proba)
+        signature = infer_signature(X_test, y_test_pred)
+        mlflow.sklearn.log_model(
+            sk_model=model,
+            artifact_path="high_custom_score_candidate",
+            signature=signature,
+            input_example=input_example
+        )
+        mlflow.set_tag("saved", "high_recall")
+        mlflow.set_tag("optuna_trial", "true")
+       
             
-            
-            # Log metrics
-            mlflow.log_metric("accuracy",accuracy)
-            mlflow.log_metric("precision", precision)
-            mlflow.log_metric("recall", recall)
-            mlflow.log_metric("f1_score", f1)
+        
+    return score
 
-            #Save the best model
-            if accuracy> best_accuracy:
-                best_accuracy =accuracy
-                best_params={n_estimators:n_estimators, max_depth:max_depth}
-                mlflow.sklearn.log_model(
-                    sk_model = model,
-                    artifact_path ="model",
-                    input_example=input_example
+study = optuna.create_study(
+    direction="maximize",
+    sampler=optuna.samplers.TPESampler(seed=42),
+    pruner=optuna.pruners.MedianPruner(n_startup_trials=10, n_warmup_steps=5)
+)
 
-                )
+with mlflow.start_run(run_name="Optuna_20_Trials_Manual_Nested"):
+    mlflow.set_tag("tuning_method", "optuna_manual_nested")
+    mlflow.set_tag("objective", "maximize_test_auc")
+    mlflow.log_param("total_trials", 20)
+
+    study.optimize(objective, n_trials=20)
+
+best_params = study.best_params
+fixed_params=({
+    "random_state": 42,
+    "n_jobs": -1,
+    "criterion": "gini"
+})
+
+with mlflow.start_run(run_name="Best_Model_Final_Optuna_Manual"):
+    mlflow.set_tag("status", "BEST_MODEL")
+    mlflow.log_params(best_params)
+    mlflow.log_params(fixed_params)
+    mlflow.log_metric("best_custom_score", study.best_value)
+    mlflow.log_metric("best_trial_number", study.best_trial.number)
+
+    # Train ulang model terbaik
+    best_model = RandomForestClassifier(**best_params, random_state=42, n_jobs=-1)
+    best_model.fit(X_train, y_train)
+
+    y_train_pred = best_model.predict(X_train)
+    y_train_proba = best_model.predict_proba(X_train)[:, 1]
+    y_test_pred  = best_model.predict(X_test)
+    y_test_proba = best_model.predict_proba(X_test)[:, 1]
+
+    # Gunakan fungsi kamu yang sudah ada
+    log_all_metrics(y_train_pred, y_train_proba, y_test_pred, y_test_proba)
+    log_visual(best_model, X_test, y_test_pred, y_test_proba)
+    log_estimator_html(best_model)
+    log_metric_info()
+
+    # Register model terbaik
+    signature = infer_signature(X_test, y_test_pred)
+    mlflow.sklearn.log_model(
+        sk_model=best_model,
+        artifact_path="model",
+        signature=signature,
+        input_example=input_example,
+        registered_model_name="Diabetes_RFC_Optuna_Manual_Best"
+    )
