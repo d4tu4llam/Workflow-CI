b1e43f9862626ad65a42231a692156c0bf315d9a
Hilmi Datu Allam <hilmi.allam@gmail.com>
2025-11-23 15:59:29 +0700

    Update modelling.py
    Menambahkan artifact dan logging
    Membuat fungsi loggin agar lebih modular

--- modelling.py
+++ modelling.py
@@ -1,14 +1,30 @@
 import mlflow
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import json
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import precision_score, recall_score, f1_score, confusion_matrix
+from sklearn.metrics import (
+    accuracy_score, precision_score, recall_score, f1_score,
+    roc_auc_score, log_loss, confusion_matrix,
+    roc_curve, precision_recall_curve, auc
+)
+from mlflow.client import MlflowClient
 import random
 import numpy as np
 import dagshub
-dagshub.init(repo_owner='d4tu4llam', repo_name='Workflow-CI', mlflow=True)
+from mlflow.models import infer_signature
+
 #tracking UI
-mlflow.set_tracking_uri("https://dagshub.com/d4tu4llam/Workflow-CI.mlflow")
+#Buat Lokal
+mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+#Buat dagshub
+#dagshub.init(repo_owner='d4tu4llam', repo_name='Workflow-CI', mlflow=True)
+#mlflow.set_tracking_uri("https://dagshub.com/d4tu4llam/Workflow-CI.mlflow")
+
+
 mlflow.set_experiment("Submission SML Modelling Diabetes Prediction Hilmi Datu Allam")
 
 train_data = pd.read_csv("diabetes_prediction_dataset_preprocessing/train_processed.csv")
@@ -22,31 +38,148 @@ y_train = train_data["diabetes"]
 X_test = test_data.drop("diabetes", axis=1)
 y_test = test_data["diabetes"]
 
-input_example = X_train[0:5]
+input_example = X_train.iloc[0:5].values
 
+def log_all_metrics(y_train_pred, y_train_proba, y_test_pred ,y_test_proba ):
+    # Log all metrics
+    mlflow.log_metrics({
+        "train_accuracy": accuracy_score(y_train, y_train_pred),
+        "train_precision": precision_score(y_train, y_train_pred),
+        "train_recall": recall_score(y_train, y_train_pred),
+        "train_f1": f1_score(y_train, y_train_pred),
+        "train_auc": roc_auc_score(y_train, y_train_proba),
+        "test_accuracy": accuracy_score(y_test, y_test_pred),
+        "test_precision": precision_score(y_test, y_test_pred),
+        "test_recall": recall_score(y_test, y_test_pred),
+        "test_f1": f1_score(y_test, y_test_pred),
+        "test_auc": roc_auc_score(y_test, y_test_proba),
+        "test_log_loss": log_loss(y_test, y_test_proba),
+    })
+ 
+def log_metric_info():
+    #Buat metric_info.json
+    metric_info = {
+        "metrics": [
+            {"name": "test_accuracy",        "type": "scalar"},
+            {"name": "test_precision",       "type": "scalar"},
+            {"name": "test_recall",          "type": "scalar"},
+            {"name": "test_f1",              "type": "scalar"},
+            {"name": "test_auc",             "type": "scalar"},
+            {"name": "test_log_loss",        "type": "scalar"},
+            {"name": "training_accuracy",    "type": "scalar"},
+            {"name": "training_precision",   "type": "scalar"},
+            {"name": "training_recall",      "type": "scalar"},
+            {"name": "training_f1",          "type": "scalar"},
+            {"name": "training_auc",         "type": "scalar"},
+            {"name": "training_log_loss",    "type": "scalar"},
+            {"name": "RandomForestClassifier_score_X_test", "type": "scalar"}
+        ]
+    }
 
+    # Simpan ke file JSON
+    with open("metric_info.json", "w") as f:
+        json.dump(metric_info, f, indent=2)
+
+    # Log ke MLflow
+    mlflow.log_artifact("metric_info.json")
+    os.remove("metric_info.json")  
+
+def log_estimator_html(model):
+    #Buat estimator.html
+    html_content = model._repr_html_()   
+
+    # Simpan ke file
+    with open("estimator.html", "w", encoding="utf-8") as f:
+        f.write(html_content)
+        
+    # Log ke MLflow
+    mlflow.log_artifact("estimator.html")
+    os.remove("estimator.html")
+
+def log_visual(model, X_test, y_test_pred, y_test_proba):
+    # estimator.html
+    with open("estimator.html", "w", encoding="utf-8") as f:
+        f.write(model._repr_html_())
+    mlflow.log_artifact("estimator.html")
+    os.remove("estimator.html")
+
+    # Confusion Matrix
+    cm = confusion_matrix(y_test, y_test_pred)
+    plt.figure(figsize=(6,5))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',
+                xticklabels=['No Diabetes', 'Diabetes'],
+                yticklabels=['No Diabetes', 'Diabetes'])
+    plt.title(f"Confusion Matrix (")
+    plt.ylabel("True"); plt.xlabel("Predicted")
+    plt.tight_layout()
+    plt.savefig(f"confusion_matrix.png"); plt.close()
+    mlflow.log_artifact(f"confusion_matrix.png")
+    os.remove(f"confusion_matrix.png")
+
+    # ROC Curve
+    fpr, tpr, _ = roc_curve(y_test, y_test_proba)
+    roc_auc_val = auc(fpr, tpr)
+    plt.figure(figsize=(7,6))
+    plt.plot(fpr, tpr, label=f'ROC Curve (AUC = {roc_auc_val:.3f})')
+    plt.plot([0,1], [0,1], 'k--')
+    plt.xlabel('FPR'); plt.ylabel('TPR'); plt.title('ROC Curve')
+    plt.legend(); plt.grid(True); plt.tight_layout()
+    plt.savefig(f"roc_curve.png"); plt.close()
+    mlflow.log_artifact(f"roc_curve.png")
+    os.remove(f"roc_curve.png")
+
+    # Precision-Recall Curve
+    precision, recall, _ = precision_recall_curve(y_test, y_test_proba)
+    pr_auc = auc(recall, precision)
+    plt.figure(figsize=(7,6))
+    plt.plot(recall, precision, label=f'PR Curve (AUC = {pr_auc:.3f})')
+    plt.xlabel('Recall'); plt.ylabel('Precision'); plt.title('Precision-Recall Curve')
+    plt.legend(); plt.grid(True); plt.tight_layout()
+    plt.savefig(f"precision_recall_curve.png"); plt.close()
+    mlflow.log_artifact(f"precision_recall_curve.png")
+    os.remove(f"precision_recall_curve.png")
 
 with mlflow.start_run():
     #Log parameters
-    n_estimators = 5
-    max_depth =5
-    mlflow.autolog()
+    params = {
+        "n_estimators": 200,
+        "max_depth": 10,
+        "min_samples_split": 2,
+        "min_samples_leaf": 1,
+        "max_features": "sqrt",
+        "random_state": 42,
+        "n_jobs": -1,
+        "bootstrap": True,
+        "class_weight": None,
+        "criterion": "gini"
+    }
+    mlflow.log_params(params)
+
     #Train model
-    model = RandomForestClassifier(n_estimators=n_estimators, max_depth = max_depth)
+    model = RandomForestClassifier(**params)
     model.fit(X_train, y_train)
-    mlflow.sklearn.log_model(
-        sk_model = model,
-        artifact_path = "model",
-        input_example = input_example
-    )
     
-    #Evaluate model
-    predictions = model.predict(X_test)
-    accuracy= model.score(X_test, y_test)
-    precision = precision_score(y_test, predictions)
-    recall = recall_score(y_test, predictions)
-    f1 = f1_score(y_test, predictions)
-    mlflow.log_metric("accuracy",accuracy)
-    mlflow.log_metric("precision", precision)
-    mlflow.log_metric("recall", recall)
-    mlflow.log_metric("f1_score", f1)
+    #Training Metrics
+    y_train_pred = model.predict(X_train)
+    y_train_proba = model.predict_proba(X_train)[:, 1]
+    #Test metrics
+    y_test_pred = model.predict(X_test)
+    y_test_proba = model.predict_proba(X_test)[:, 1]
+    #Log all metrics
+    log_all_metrics(y_train_pred, y_train_proba, y_test_pred, y_test_proba)
+    #Log metric info
+    log_metric_info()
+    #Log Estimator
+    log_estimator_html(model)
+    #Log visual seperti cm roc auc 
+    log_visual(model, X_test, y_test_pred, y_test_proba)
+
+    
+    signature = infer_signature(X_test, y_test_pred)
+
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model",
+        signature=signature,
+        input_example=input_example
+    )
